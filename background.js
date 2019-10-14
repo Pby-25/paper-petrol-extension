97da5f6af615569f6385be766a89f258be2e2436
Pby-25 <agains1595@gmail.com>
2019-10-13 22:34:23 -0700

    further improve messaging

--- background.js
+++ background.js
@@ -5,11 +5,39 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 
 chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse)=>{
+  console.log("a new message from...")
+  console.log(sender)
+  console.log(request)
+
+  console.log("reducing")
+  request.reduce((resPromise, station)=>{
+    return resPromise.then( (gasPrices) => {
+      return new Promise((resolve)=>{
+        setTimeout(function () {
+          console.log(station);
+          resolve();
+      }, 1000)
+    })
+  })
+  }, Promise.resolve());
+
+// request.reduce( (p, station, i) => 
+//     p.then(_ => new Promise(resolve =>
+//         setTimeout(function () {
+//             console.log(i);
+//             console.log(station);
+//             resolve();
+//         }, 1000)
+//     ))
+// , Promise.resolve() );
 
 })
+
+
 const INTERVAL = 5000;
 setTimeout(function(){
-    chrome.tabs.create({url: "https://goo.gl/maps/BNtLNQc7AUzu4rkF8", active: false }, (tab) =>{
+    chrome.tabs.create({active: false}, (tab) =>{
+      chrome.tabs.update(tab.id, {url: "https://goo.gl/maps/BNtLNQc7AUzu4rkF8"}, () =>{
         setTimeout(function(){
           chrome.tabs.executeScript(tab.id, {file: "paperpetrol.js"}, ()=>{
             chrome.tabs.sendMessage(tab.id, {newLink: true}, (response)=> {
@@ -18,6 +46,8 @@ setTimeout(function(){
             })
           })
             // chrome.tabs.remove(tab.id);
-        },INTERVAL);
+        },5000);
+      })
+        
     }); 
-},INTERVAL);
+},10);
